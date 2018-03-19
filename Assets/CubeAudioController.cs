b2b073f59188fccb69450baaa32bd2a5a6183f09
erikoichiki <aptrk.usam@gmail.com>
2018-03-19 16:45:05 +0900

    レッスン7の課題
    ブロックの効果音をつけました。

---
+++ Assets/CubeAudioController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeAudioController : MonoBehaviour {
+
+	AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () { 
+		
+		 audioSource = GetComponent<AudioSource> ();
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.tag == "CubePrefabTag" || other.gameObject.tag=="groundTag") {
+		 
+
+			audioSource.PlayOneShot(audioSource.clip);
+		}
+		}
+	
+}
+
